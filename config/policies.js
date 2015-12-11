80c371f2711dd3447effc9be0e3f17bc53b2f96e
sc.k.nechaev <k.nechaev@streamcrease.com>
2015-12-11 17:48:22 +0200

    Add admin checking for all routes for admin

--- config/policies.js
+++ config/policies.js
@@ -49,8 +49,7 @@ module.exports.policies = {
     // }
 
     'AdminController': {
-        // '*': 'isAdmin'
-        '*'     : true,
+        '*': 'isAdmin'
     },
     'UserController': {
         '*'     : 'isAdmin',
