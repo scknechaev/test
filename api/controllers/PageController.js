66b0199fbcbff135dcc8102c71a4d08e1480bf0a
sc.k.nechaev <k.nechaev@streamcrease.com>
2015-12-15 19:24:35 +0200

    Add vieweing of the pages, add folder for pages

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -1,7 +1,32 @@
 module.exports = {
-	render: function (req, res, next) {
+	
+	render: function (req, res) {
 		return res.view('mainBook', {
            layout: false
        });
+	},
+
+	getPage: function (req, res) {
+		var url = req.param('pageUrl');
+		
+		Page.findOne({
+			'url': url
+		}).exec(function (err, page) {
+			if (err || !page) {
+
+				if (!page) {
+					return res.notFound();
+				} 
+
+				return res.badRequest(err); 
+			}
+
+			res.render('./page', {
+				'body' : page.html,
+				'title': page.title
+			});
+
+		});
 	}
+
 };
