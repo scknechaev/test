34b4fa52fd439faea2197e05077de631578e36df
alexkushnarov <a.kushnarov@streamcrease.com>
2015-12-18 21:31:19 +0200

    added media content

--- assets/cmsAdmin/assets/js/controllers/mediaController.js
+++ assets/cmsAdmin/assets/js/controllers/mediaController.js
@@ -1,6 +1,22 @@
 angular.module('app')
-	.controller('mediaController', ['$scope', '$modal',
- 	function ($scope, $modal) {
+	.controller('mediaController', ['$scope', '$modal', '$http', '$sce',
+ 	function ($scope, $modal, $http, $sce) {
+    $scope.mediaArr = [];
+    function getMedia() {
+        $http.get('/media')
+        .then(function(res){
+            $scope.mediaArr = res.data;
+            processVideo();
+        })
+    }
+    function processVideo(){
+        for (var i = 0; i < $scope.mediaArr.length; i++) {
+            if($scope.mediaArr[i].type === 'video'){
+                $scope.mediaArr[i].url = $sce.trustAsResourceUrl($scope.mediaArr[i].url)
+            }
+        };
+    }
+    getMedia();
     $scope.showMediaItem = function(item) {
     	$modal.open({
     	    resolve:{
@@ -8,6 +24,7 @@ angular.module('app')
     	        return item;
     	      }
     	    },
+            size: 'lg',
     	    templateUrl: 'cmsAdmin/tpl/modals/showMediaItem.html.tmpl',
     	    controller: ['$scope', '$modalInstance', 'item',
     	      function($scope, $modalInstance, item) {
