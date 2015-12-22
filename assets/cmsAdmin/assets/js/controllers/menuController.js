7408747dbb5c279a1c779d28f987f7382057dac4
alexkushnarov <a.kushnarov@streamcrease.com>
2015-12-22 13:41:34 +0200

    changed page table and removed add buttons from 2 level nav

--- assets/cmsAdmin/assets/js/controllers/menuController.js
+++ assets/cmsAdmin/assets/js/controllers/menuController.js
@@ -78,61 +78,7 @@ angular.module('app')
                   duration: 2500
             });
           })
-            
         }
-
-        // $scope.setModalState = function (editingNav) {
-        //     $scope.Form.navModalForm.name.$setPristine();
-        //     $scope.Form.navModalForm.name.$setUntouched();
-        //     $scope.isEditing = false;
-
-        //     if (editingNav) {
-        //         $scope.isEditing = true;
-        //         $scope.nav       = editingNav;
-        //         $scope.navCopy   = Object.create(editingNav);
-        //     } else {
-        //         $scope.navCopy = {};
-        //     }
-
-        //     resetModalState($scope.navModal);
-        // };
-
-        // $scope.delNav = function (nav, index) {
-        //     navigationService.delNav(nav).then (function (data) {
-        //         $scope.navs.splice(index, 1);
-        //     }, function (err) {
-        //         console.log('error deleting nav');
-        //     })
-        // };
-        // $scope.openConfirmDelNav = function (nav, index) {
-        //   $modal.open({
-        //       templateUrl: 'cmsAdmin/tpl/modals/confirmDelNav.html.tmpl',
-        //       resolve: {
-        //         nav : function () {
-        //           return nav;
-        //         },
-        //         index : function () {
-        //           return index;
-        //         }
-        //       },
-        //       controller: ['$scope', '$modalInstance', 'nav', 'index', function($scope, $modalInstance, nav, index) {
-        //           $scope.index = index;
-        //           $scope.nav =  nav;
-        //           setTimeout(function(){
-        //               $('.modal-footer .btn.btn-danger').focus();
-        //           },200);
-        //           $scope.cancel = function () {
-        //               $modalInstance.dismiss('cancel');
-        //           };
-        //       }]
-        //   });
-        // };
-
-        // function resetModalState (modalWindow) {
-        //     modalWindow.alert       = false;
-        //     modalWindow.modalClosed = false;
-        // }
-
         
 }]);
 
